40c76bcb468e1ecc029fa77526b2ed20339ba728
cjl_macbook <cjl_spy@163.com>
2019-12-22 11:07:11 +0800

    增加了一个按钮

--- lib/new_navigator.dart
+++ lib/new_navigator.dart
@@ -9,24 +9,30 @@ class _NewRouteRootState extends State<NewRouteRoot> {
   @override
   Widget build(BuildContext context) {
     BuildContext c;
-    return WillPopScope(
-      onWillPop: () async {
-        final result = await Navigator.maybePop(c);
-        if (!result) {
-          Navigator.pop(context);
-        }
-        return false;
+    return NotificationListener<_BackNotfication>(
+      onNotification: (_) {
+        Navigator.pop(context);
+        return true;
       },
-      child: Scaffold(
-        body: Navigator(
-          onGenerateRoute: (s) {
-            return MaterialPageRoute(
-              builder: (BuildContext context) {
-                c = context;
-                return NewPage();
-              },
-            );
-          },
+      child: WillPopScope(
+        onWillPop: () async {
+          final result = await Navigator.maybePop(c);
+          if (!result) {
+            Navigator.pop(context);
+          }
+          return false;
+        },
+        child: Scaffold(
+          body: Navigator(
+            onGenerateRoute: (s) {
+              return MaterialPageRoute(
+                builder: (BuildContext context) {
+                  c = context;
+                  return NewPage();
+                },
+              );
+            },
+          ),
         ),
       ),
     );
@@ -42,7 +48,13 @@ class _NewPageState extends State<NewPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: BackButton(
+          onPressed: () {
+            _BackNotfication().dispatch(context);
+          },
+        ),
+      ),
       body: Container(
         child: Text("新路由"),
       ),
@@ -73,3 +85,5 @@ class _Page2State extends State<Page2> {
     );
   }
 }
+
+class _BackNotfication extends Notification {}
